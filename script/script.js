031be49312edb813abb4059af527b23037d1033b
DevKauagm <kauag0988@gmail.com>
2025-10-13 18:20:41 -0300

    up

--- script/script.js
+++ script/script.js
@@ -1,12 +1,25 @@
 const produtos = {
-    eletrodom: {geladeira: [{nome: 'Geladeira Electrolux Frost Free Duplex 310 Litros', pr: 1.403, imgs: ['imagens/eletrodomestico/geladeira/Geladeira_Electrolux_Frost_Free_Duplex_310Litros-1.jpg']}, {}], tv: [{}]}
+    eletrodom: {geladeira: [{nome: 'Geladeira Electrolux Frost Free Duplex 310 Litros', pr: 1.403, imgs: ['imagens/eletrodomestico/geladeira/Geladeira_Electrolux_Frost_Free_Duplex_310Litros-1.jpg', 'imagens/eletrodomestico/geladeira/Geladeira_Electrolux_Frost_Free_Duplex_310Litros-2.jpg', 'imagens/eletrodomestico/geladeira/Geladeira_Electrolux_Frost_Free_Duplex_310Litros-3.png', 'imagens/eletrodomestico/geladeira/Geladeira_Electrolux_Frost_Free_Duplex_310Litros-4.jpg']}, {}], tv: [{}]}
 }
 
 const section_eletro = document.querySelectorAll('#eletrodomesticos > section')
 
+const img = document.querySelectorAll('main > img')
+
+function fotos (nome, pr, imgs) {
+    document.querySelector('main').style.display = 'none'
+    document.body.style.backgroundColor = 'white'
+    document.querySelector('#pgproduto').style.display = 'grid'
+    for (let i = 0; i <= 3; i++) {
+        img[i].src = imgs[i]
+    }
+    document.querySelector('#pgproduto > h1').innerText = nome
+    document.querySelector('#pgproduto > h2').innerText = `R$ ${pr}`
+}
+
 for (let i in produtos.eletrodom.geladeira) {
     const img = document.createElement('img')
     img.setAttribute('src', produtos.eletrodom.geladeira[i].imgs[0])
-    console.log(produtos.eletrodom.geladeira[i].imgs[0])
     section_eletro[0].appendChild(img)
+    img.addEventListener('click', () => fotos(produtos.eletrodom.geladeira[i].nome, produtos.eletrodom.geladeira[i].pr, produtos.eletrodom.geladeira[i].imgs))
 }

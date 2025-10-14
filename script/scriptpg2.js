6c5772dec0fe683e1f50382effeed71e16cbc40f
DevKauagm <kauag0988@gmail.com>
2025-10-14 20:49:28 -0300

    up

--- script/scriptpg2.js
+++ script/scriptpg2.js
@@ -1,11 +1,18 @@
 const info = JSON.parse(localStorage.getItem('info'))
-const img = document.querySelectorAll('section > img')
+const imgs = document.querySelectorAll('section > img')
 const h1 = document.querySelector('section > h1')
 const h2 = document.querySelector('section > h2')
 
+function img_principal(img) {
+    for (const imagem of imgs) {
+        imagem.removeAttribute('id')
+    }
+    img.setAttribute('id', 'principal')
+}
+
 for (let i = 0; i <= 3; i++) {
-    img[i].src = info.imgs[i]
-    img[i].addEventListener('click')
+    imgs[i].src = info.imgs[i]
+    imgs[i].addEventListener('click', () => img_principal(imgs[i]))
     h1.innerText = info.nome
     h2.innerText = `R$${info.pr}`
 }
